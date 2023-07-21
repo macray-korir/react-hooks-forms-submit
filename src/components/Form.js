0e8d889b1d9258fa7c623fcfc2a33cf0a9727b54
macray <macraykorir254@gmail.com>
2023-07-21 14:43:22 +0300

    Handle form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,46 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Perform input validation
+    if (firstName.trim() === "" || lastName.trim() === "") {
+      setErrors(["First name and last name are required!"]);
+    } else {
+      const formData = { firstName: firstName, lastName: lastName };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]); // Clear errors if the form submission is successful
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* Conditionally render error messages */}
+      {errors.length > 0 &&
+        errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
